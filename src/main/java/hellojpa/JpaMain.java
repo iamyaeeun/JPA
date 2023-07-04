237be9674da900795a62a428c25f16e68d24edc5
허예은 <20211415@sungshin.ac.kr>
2023-07-04 20:40:23 +0900

    준영속 상태

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,13 +18,12 @@ public class JpaMain {
 
         try{
             //영속
-            Member member=new Member(200L,"member200");
-            em.persist(member);
+            Member member=em.find(Member.class,150L);
+            member.setName("AAAAA");
 
-            em.flush(); //flush는 영속성 컨텍스트의 변경내용을 DB에 동기화(트랜잭션 commit 시 flush 자동 호출됨)
+            em.detach(member); //준영속 상태(영속성 컨텍스트에서 분리했으므로 위에서 setName으로 데이터 변경했어도 트랜잭션 commit 시 update 쿼리 안나감)
 
             System.out.println("===============");
-
             tx.commit(); //em.persist(member) 할때가 아닌 commit 할때 DB에 member(엔티티) 저장됨
         } catch (Exception e){
             tx.rollback();
